6a02e6e895a0032966539ce42a3edbad4e42b4ca
not a cat <pandinobud@gmail.com>
2023-07-16 18:28:15 -0700

    [ft] element insertion + cleaning up ryan's stinky code

--- main.py
+++ main.py
@@ -6,25 +6,39 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import date, datetime, timezone
 import pylunar
 import data
-import time
+import elements
+
 
 def play_game():
     driver = launch_page()
     password = generate_password()
     box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ProseMirror")))
-    driver.execute_script("arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">"+ password +"</span></p>'", box)
+    driver.execute_script(
+        "arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">" + password +
+        "</span></p>'",
+        box)
 
     captcha = get_captcha_answer(driver)
     password = generate_password(captcha)
-    driver.execute_script("arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">"+ password +"</span></p>'", box)
+    driver.execute_script(
+        "arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">" + password +
+        "</span></p>'",
+        box)
 
     location = get_location(driver).lower()
     password = generate_password(captcha, location)
-    driver.execute_script("arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">"+ password +"</span></p>'", box)
+    driver.execute_script(
+        "arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">" + password +
+        "</span></p>'",
+        box)
 
     chess_move = get_chess_move(driver)
     password = generate_password(captcha, location, chess_move)
-    driver.execute_script("arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">"+ password +"</span></p>'", box)
+    driver.execute_script(
+        "arguments[0].innerHTML = '<p><span style=\"font-family: Monospace; font-size: 28px\">" + password +
+        "</span></p>'",
+        box)
+
 
 def generate_password(captcha='', location='', chess_move=''):
     # 5: digits must add to 25
@@ -32,30 +46,36 @@ def generate_password(captcha='', location='', chess_move=''):
     # 8: needs to include a sponsor
     # 9: needs to multiply to 35
     # 10: captcha
-    sponsor = 'sHell' # somewhat mandatory
-    month = 'may' # somewhat mandatory
-    thiry_five_mult = 'XXXV' # mandatory
-    wordle = get_wordle_answer() # mandatory
-    moon = get_moon_phase() # mandatory
+    sponsor = 'sHell'  # somewhat mandatory
+    month = 'may'  # somewhat mandatory
+    thiry_five_mult = 'XXXV'  # mandatory
+    wordle = get_wordle_answer()  # mandatory
+    moon = get_moon_phase()  # mandatory
     leap_year = '0'  # mandatory
+    atomic_number_requirement = 200
 
-    updated_password = data.paul + sponsor + month + thiry_five_mult + wordle + moon + leap_year + captcha + location + chess_move
-    sum = sum_25(updated_password)
+    updated_password = data.paul + leap_year + sponsor + month + thiry_five_mult + wordle + moon + captcha + location \
+                       + chess_move
+    updated_password += sum_25(updated_password)
+    updated_password += elements.required_elements_str(atomic_number_requirement
+                                                       - elements.password_element_sum(updated_password))
 
-    return data.paul + sponsor + month + thiry_five_mult + wordle + moon + leap_year + captcha + location + chess_move + sum
+    return updated_password
+
 
 def launch_page():
     driver = webdriver.Firefox()
     driver.get("https://neal.fun/password-game/")
     return driver
 
+
 def sum_25(password):
     # sum all the digits in the string and return the difference from 25
-    sum = 0
+    le_sum = 0
     for char in password:
         if char.isdigit():
-            sum += int(char)
-    diff = 25 - sum
+            le_sum += int(char)
+    diff = 25 - le_sum
     return_str = ""
     while diff > 9:
         return_str += "9"
@@ -63,10 +83,12 @@ def sum_25(password):
     return_str += str(diff)
     return return_str
 
+
 def get_wordle_answer():
     # uses requests library
-    response = requests.get("https://neal.fun/api/password-game/wordle?date="+date.today().strftime("%Y-%m-%d"))
+    response = requests.get("https://neal.fun/api/password-game/wordle?date=" + date.today().strftime("%Y-%m-%d"))
     return response.json()['answer']
+
 
 def get_captcha_answer(driver):
     # explicit wait
@@ -74,14 +96,16 @@ def get_captcha_answer(driver):
     # get src image attribute
     return element.get_attribute("src").split("/")[-1].split(".")[0]
 
+
 def get_moon_phase():
     # uses pylunar library
-    mi = pylunar.MoonInfo((0,0,0),(0,0,0))
+    mi = pylunar.MoonInfo((0, 0, 0), (0, 0, 0))
     # use UTC
     utc = datetime.now(timezone.utc)
     datetime_tuple = (utc.year, utc.month, utc.day, utc.hour, utc.minute, utc.second)
     mi.update(datetime_tuple)
     return data.lunar_dict[mi.phase_name()]
+
 
 def get_location(driver):
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "geo-wrapper")))
@@ -90,9 +114,11 @@ def get_location(driver):
     # search for link in maps.jsonc and return location
     return data.locations[link]
 
+
 def get_chess_move(driver):
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "chess-img")))
     index = element.get_attribute("src").split("puzzle")[-1].split(".")[0]
     return data.moves[int(index)]
+
 
 play_game()
